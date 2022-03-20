99389d59a1bd6f9eb3a49bf07aaad4f056f7a5b4
haikallf <haikallzrd@gmail.com>
2022-03-20 19:01:39 +0700

    add screenshots, serial.c main program fix

--- src/serial.c
+++ src/serial.c
@@ -232,10 +232,11 @@ void print_array(int *n, int size)
 int get_median(int *n, int length)
 {
     int mid = length / 2;
-    if (length & 1) {
+    if (length & 1)
+    {
         return n[mid];
     }
- 
+
     return (n[mid - 1] + n[mid]) / 2;
 }
 
@@ -251,4 +252,47 @@ long get_floored_mean(int *n, int length)
     }
 
     return sum / length;
+}
+
+// main() driver
+int main()
+{
+    int kernel_row, kernel_col, target_row, target_col, num_targets;
+    clock_t start = clock();
+
+    // reads kernel's row and column and initalize kernel matrix from input
+    scanf("%d %d", &kernel_row, &kernel_col);
+    Matrix kernel = input_matrix(kernel_row, kernel_col);
+
+    // reads number of target matrices and their dimensions.
+    // initialize array of matrices and array of data ranges (int)
+    scanf("%d %d %d", &num_targets, &target_row, &target_col);
+    Matrix *arr_mat = (Matrix *)malloc(num_targets * sizeof(Matrix));
+    int arr_range[num_targets];
+
+    // read each target matrix, compute their convolution matrices, and compute their data ranges
+    for (int i = 0; i < num_targets; i++)
+    {
+        arr_mat[i] = input_matrix(target_row, target_col);
+        arr_mat[i] = convolution(&kernel, &arr_mat[i]);
+        arr_range[i] = get_matrix_datarange(&arr_mat[i]);
+    }
+
+    // sort the data range array
+    merge_sort(arr_range, 0, num_targets - 1);
+
+    int median = get_median(arr_range, num_targets);
+    int floored_mean = get_floored_mean(arr_range, num_targets);
+
+    // print the min, max, median, and floored mean of data range array
+    printf("min: %d\nmax: %d\nmedian: %d\nfloored mean: %d\n",
+           arr_range[0],
+           arr_range[num_targets - 1],
+           median,
+           floored_mean);
+
+    double duration = (double)(clock() - start) / CLOCKS_PER_SEC;
+    printf("Processing Time: %f\n", duration);
+
+    return 0;
 }
